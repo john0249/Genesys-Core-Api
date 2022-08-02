178db95074f02d9e5e1e5e2cac0543c1bf6ef528
john0 <jcampusano@k3d.do>
2022-08-01 22:58:21 -0400

    Add existing project files to Git

---
+++ Models/GenesysCoreEntities.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Genesys_Core_API.Models
+{
+    public partial class GenesysCoreEntities : DbContext
+    {
+        public GenesysCoreEntities() : base()
+        {
+
+        }
+
+        public virtual DbSet<Users>? Users { get; set; }
+        
+    }
+}
